853ab8376e6d7de2513bf5e2935574c1a28bbae8
Johan R <johanreza58@gmail.com>
2023-04-05 21:44:50 +0700

    Berhasil mengambil data dari NewsApi Berdasarkan masing-masing category

---
+++ src/js/main.js
@@ -0,0 +1,116 @@
+function main() {
+  const getNews = (url) => {
+    // terdapat nilai null dari API
+    fetch(url, {
+      method: "GET",
+      headers: {
+        "X-Api-Key": "a401d3edd5e6462db1c119c1301a27af",
+      },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseJson) => {
+        if (responseJson.error) {
+          showResponseMessage(responseJson.message);
+        } else {
+          renderAllNews(responseJson.articles);
+        }
+      })
+      .catch((error) => {
+        showError(error);
+      });
+  };
+
+  const renderAllNews = (News) => {
+    const listNewsElement = document.getElementById("listNews");
+    listNewsElement.innerHTML = "";
+
+    News.forEach((news) => {
+      listNewsElement.innerHTML += `<div class="col">
+        <div class="card">
+          <img src="${news.urlToImage}" class="card-img-top" alt="image" />
+          <div class="card-body">
+            <a href="${news.url}"><h5 class="card-title">${news.title}</h5></a>
+            <h6 class="card-title">${news.author}</h6>
+            <p class="card-text">${news.description}</p>
+          </div>
+        </div>
+      </div>`;
+    });
+  };
+
+  const showError = () => {
+    document.body.innerHTML += `<div class="m-auto position-absolute top-50 start-50 translate-middle">
+    <h1 class="text-center">Not Found</h1>
+    <h1 class="text-center">Check your internet connection</h1>
+  </div>`;
+  };
+
+  document.addEventListener("DOMContentLoaded", () => {
+    let baseUrlCategory = "https://newsapi.org/v2/top-headlines?country=us&category=";
+
+    const catGeneral = document.getElementById("categoryGeneral");
+    const catBusiness = document.getElementById("categoryBusiness");
+    const catEntertainment = document.getElementById("categoryEntertainment");
+    const catHealth = document.getElementById("categoryHealth");
+    const catScience = document.getElementById("categoryScience");
+    const catSports = document.getElementById("categorySports");
+    const catTechnology = document.getElementById("categoryTechnology");
+    const inputSearch = document.getElementById("searchInput");
+    const buttonInputSearch = document.getElementById("buttonSearch");
+
+    catGeneral.addEventListener("click", (event) => {
+      event.preventDefault();
+      const category = "general";
+      baseUrlCategory = baseUrlCategory.replace(/category=[^&]*/, `category=${category}`);
+      getNews(baseUrlCategory);
+    });
+    catBusiness.addEventListener("click", (event) => {
+      event.preventDefault();
+      const category = "business";
+      baseUrlCategory = baseUrlCategory.replace(/category=[^&]*/, `category=${category}`);
+      getNews(baseUrlCategory);
+    });
+    catEntertainment.addEventListener("click", (event) => {
+      event.preventDefault();
+      const category = "entertainment";
+      baseUrlCategory = baseUrlCategory.replace(/category=[^&]*/, `category=${category}`);
+      getNews(baseUrlCategory);
+    });
+    catHealth.addEventListener("click", (event) => {
+      event.preventDefault();
+      const category = "health";
+      baseUrlCategory = baseUrlCategory.replace(/category=[^&]*/, `category=${category}`);
+      getNews(baseUrlCategory);
+    });
+    catScience.addEventListener("click", (event) => {
+      event.preventDefault();
+      const category = "science";
+      baseUrlCategory = baseUrlCategory.replace(/category=[^&]*/, `category=${category}`);
+      getNews(baseUrlCategory);
+    });
+    catSports.addEventListener("click", (event) => {
+      event.preventDefault();
+      const category = "sports";
+      baseUrlCategory = baseUrlCategory.replace(/category=[^&]*/, `category=${category}`);
+      getNews(baseUrlCategory);
+    });
+    catTechnology.addEventListener("click", (event) => {
+      event.preventDefault();
+      const category = "technology";
+      baseUrlCategory = baseUrlCategory.replace(/category=[^&]*/, `category=${category}`);
+      getNews(baseUrlCategory);
+    });
+
+    buttonInputSearch.addEventListener("click", () => {
+      const news = {
+        title: inputSearch.ariaValueMax,
+      };
+    });
+
+    getNews(baseUrlCategory);
+  });
+}
+
+export default main;
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/js/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: () => [require("autoprefixer")],
+              },
+            },
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+  ],
+};
